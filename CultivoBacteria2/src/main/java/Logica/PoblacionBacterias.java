a355124762e78e77cf16be64c1313b6e0684d446
KravAD <dkrav@myuax.com>
2024-05-17 20:26:27 +0200

    commit 3

--- CultivoBacteria2/src/main/java/Logica/PoblacionBacterias.java
+++ CultivoBacteria2/src/main/java/Logica/PoblacionBacterias.java
@@ -30,56 +30,28 @@ public class PoblacionBacterias implements Serializable {
         return nombre;
     }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
     public String getFechaInicio() {
         return fechaInicio;
-    }
-
-    public void setFechaInicio(String fechaInicio) {
-        this.fechaInicio = fechaInicio;
     }
 
     public String getFechaFin() {
         return fechaFin;
     }
 
-    public void setFechaFin(String fechaFin) {
-        this.fechaFin = fechaFin;
-    }
-
     public int getNumeroInicialBacterias() {
         return numeroInicialBacterias;
-    }
-
-    public void setNumeroInicialBacterias(int numeroInicialBacterias) {
-        this.numeroInicialBacterias = numeroInicialBacterias;
     }
 
     public int getTemperatura() {
         return temperatura;
     }
 
-    public void setTemperatura(int temperatura) {
-        this.temperatura = temperatura;
-    }
-
     public String getCondicionesLuminosidad() {
         return condicionesLuminosidad;
     }
 
-    public void setCondicionesLuminosidad(String condicionesLuminosidad) {
-        this.condicionesLuminosidad = condicionesLuminosidad;
-    }
-
     public List<Integer> getDosisComida() {
         return dosisComida;
-    }
-
-    public void setDosisComida(List<Integer> dosisComida) {
-        this.dosisComida = dosisComida;
     }
 
     private List<Integer> calcularDosisComida(int dosisComidaInicial, int diaIncremento, int dosisComidaDiaIncremento, int dosisComidaFinal) {
