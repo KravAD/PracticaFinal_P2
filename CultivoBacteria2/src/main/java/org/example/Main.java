e4e9614c4f872b9c048c7a5f6123dd63172c3175
KravAD <dkrav@myuax.com>
2024-05-18 22:31:05 +0200

    commit 36
    Añado la instancia de la clase InterfazMontecarlo que contiene la interfaz de usuario. también establezco la simulación a 30 días.

--- CultivoBacteria2/src/main/java/org/example/Main.java
+++ CultivoBacteria2/src/main/java/org/example/Main.java
@@ -1,9 +1,13 @@
 package org.example;
 import IU.*;
+import Montecarlo.Simulacion;
 
 public class Main {
     public static void main(String[] args) {
         new Interfaz();
+
+        Simulacion simulacion = new Simulacion( 30);
+        new InterfazMontecarlo(simulacion);
 
     }
 }
