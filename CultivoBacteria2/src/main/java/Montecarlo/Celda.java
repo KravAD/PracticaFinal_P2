204516b1f59961e687f71f8cb7c0bafec9c94529
KravAD <dkrav@myuax.com>
2024-05-18 18:40:42 +0200

    commit 20
    me he equivocado de clase

--- CultivoBacteria2/src/main/java/Montecarlo/Celda.java
+++ CultivoBacteria2/src/main/java/Montecarlo/Celda.java
@@ -1,4 +1,14 @@
 package Montecarlo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Celda {
+    private int comida;
+    private List<Bacteria> bacterias;
+
+    public Celda(){
+        this.comida = comida;
+        this.bacterias = new ArrayList<Bacteria>();
+    }
 }
--- CultivoBacteria2/src/main/java/Montecarlo/Simulacion.java
+++ CultivoBacteria2/src/main/java/Montecarlo/Simulacion.java
@@ -1,14 +1,6 @@
 package Montecarlo;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class Simulacion {
-    private int comida;
-    private List<Bacteria> bacterias;
 
-    public Celda(){
-        this.comida = comida;
-        this.bacterias = new ArrayList<Bacteria>();
-    }
 }
