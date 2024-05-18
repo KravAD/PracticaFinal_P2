3764ecebab099f3148988700d9e645b904217f2a
KravAD <dkrav@myuax.com>
2024-05-18 12:21:20 +0200

    commit 9
    Método del calculo de comida de modo constante. Da una cantidad constante durante el experimento.

--- CultivoBacteria2/src/main/java/Logica/EstrategiaComidaConstante.java
+++ CultivoBacteria2/src/main/java/Logica/EstrategiaComidaConstante.java
@@ -1,4 +1,15 @@
 package Logica;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class EstrategiaComidaConstante implements EstrategiaComida{
+    @Override
+    public List<Integer> calcularDosisComida(PoblacionBacterias poblacion) {
+        List<Integer> dosisComida = new ArrayList<>();
+        for (int i = 0; i < poblacion.getDuracion(); i++) {
+            dosisComida.add(poblacion.getDosisComida().get(0));
+        }
+        return dosisComida;
+    }
 }
