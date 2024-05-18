e2007faeaaba2525374746139e8671833c515bdd
KravAD <dkrav@myuax.com>
2024-05-18 20:32:44 +0200

    commit 29
    Añado los atributos de filas, columnas y plato de cultivo y los métodos de mover que hace la acción de mover la bacteria y obtener celda adyacente que dirá a que celda se moverá.

--- CultivoBacteria2/src/main/java/Montecarlo/Celda.java
+++ CultivoBacteria2/src/main/java/Montecarlo/Celda.java
@@ -6,10 +6,16 @@ import java.util.List;
 public class Celda {
     private int comida;
     private List<Bacteria> bacterias;
+    private int fila;
+    private int columna;
+    private Plato platoDeCultivo;
 
-    public Celda(){
+    public Celda(int fila, int columna, Plato platoDeCultivo){
         this.comida = comida;
         this.bacterias = new ArrayList<Bacteria>();
+        this.fila = fila;
+        this.columna = columna;
+        this.platoDeCultivo = platoDeCultivo;
     }
 
     public int getComida() {
@@ -26,5 +32,39 @@ public class Celda {
 
     public void setBacterias(List<Bacteria> bacterias) {
         this.bacterias = bacterias;
+    }
+    public void moverBacteria (Bacteria bacteria, int direccion){
+        bacterias.remove(bacteria);
+
+        Celda celdaAdyacente = obtenerCeldaAdyacente(direccion);
+
+        celdaAdyacente.getBacterias().add(bacteria);
+
+        bacteria.setCeldaActual(celdaAdyacente);
+    }
+    public Celda obtenerCeldaAdyacente(int direccion) {
+        int filaActual = this.fila;
+        int columnaActual = this.columna;
+
+        switch (direccion) {
+            case 0:
+                filaActual--;
+                break;
+            case 1:
+                columnaActual++;
+                break;
+            case 2:
+                filaActual++;
+                break;
+            case 3:
+                columnaActual--;
+                break;
+        }
+
+        if (filaActual >= 0 && filaActual < platoDeCultivo.getFilas() && columnaActual >= 0 && columnaActual < platoDeCultivo.getColumnas()) {
+            return platoDeCultivo.getCelda(filaActual, columnaActual);
+        } else {
+            return this;
+        }
     }
 }
