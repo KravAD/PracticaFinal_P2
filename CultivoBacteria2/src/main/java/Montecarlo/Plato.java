a2cf983a61a277127383b6cb45f461192fe03b46
KravAD <dkrav@myuax.com>
2024-05-18 20:23:46 +0200

    commit 27
    En la clase Plato establezco atributo celdas y en el constructor las especifico a  filas y columnas.

--- CultivoBacteria2/src/main/java/Montecarlo/Plato.java
+++ CultivoBacteria2/src/main/java/Montecarlo/Plato.java
@@ -1,4 +1,26 @@
 package Montecarlo;
 
 public class Plato {
+    private Celda[][] celdas;
+
+    public Plato(int filas, int columnas) {
+        celdas = new Celda[filas][columnas];
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                celdas[i][j] = new Celda(i, j, this);
+            }
+        }
+    }
+
+    public int getFilas() {
+        return celdas.length;
+    }
+
+    public int getColumnas() {
+        return celdas[0].length;
+    }
+
+    public Celda getCelda(int fila, int columna) {
+        return celdas[fila][columna];
+    }
 }
