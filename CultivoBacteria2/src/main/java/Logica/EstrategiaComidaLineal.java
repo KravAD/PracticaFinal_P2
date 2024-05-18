0b0f00ac16c9a0d9ef202cc7f5dc5352d7a0beb9
KravAD <dkrav@myuax.com>
2024-05-18 12:25:36 +0200

    commit 10
    Método de cálculo de dosis de manera lineal. Se toma la comida inicial, final y duración del experimento para calcular la dosis de cada día.

--- CultivoBacteria2/src/main/java/Logica/EstrategiaComidaLineal.java
+++ CultivoBacteria2/src/main/java/Logica/EstrategiaComidaLineal.java
@@ -1,4 +1,18 @@
 package Logica;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class EstrategiaComidaLineal implements EstrategiaComida{
+    @Override
+    public List<Integer> calcularDosisComida(PoblacionBacterias poblacion) {
+        List<Integer> dosisComida = new ArrayList<>();
+        int dosisInicial = poblacion.getDosisComida().get(0);
+        int dosisFinal = poblacion.getDosisComida().get(1);
+        int incremento = (dosisFinal - dosisInicial) / (poblacion.getDuracion() - 1);
+        for (int i = 0; i < poblacion.getDuracion(); i++) {
+            dosisComida.add(dosisInicial + i * incremento);
+        }
+        return dosisComida;
+    }
 }
