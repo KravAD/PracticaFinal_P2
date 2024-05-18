617ee2c76899dbba4270abb1ae59bc8cb43df21c
KravAD <dkrav@myuax.com>
2024-05-18 18:53:21 +0200

    commit 22
    Añado atributo para identificar celda y tres métodos: comer, mover y reproducir.

--- CultivoBacteria2/src/main/java/Montecarlo/Bacteria.java
+++ CultivoBacteria2/src/main/java/Montecarlo/Bacteria.java
@@ -2,8 +2,20 @@ package Montecarlo;
 
 public class Bacteria {
     private int comidaIngerida;
+    private Celda celdaActual;
 
     public Bacteria(){
         this.comidaIngerida = comidaIngerida;
     }
+
+    public void mover(){
+
+    }
+    public void comer(){
+
+    }
+    public void reproducir(){
+
+    }
+
 }
