b3e7337a310c4bae575e93db747601063d42be0d
KravAD <dkrav@myuax.com>
2024-05-18 12:26:45 +0200

    commit 11
    Método de cálculo de dosis alternativa. Mira si el día es par o impar y le asigna la dosis o nada. En este caso 0 a los días impares.

--- CultivoBacteria2/src/main/java/Logica/EstrategiaComidaAlternante.java
+++ CultivoBacteria2/src/main/java/Logica/EstrategiaComidaAlternante.java
@@ -1,4 +1,19 @@
 package Logica;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class EstrategiaComidaAlternante implements EstrategiaComida{
+    @Override
+    public List<Integer> calcularDosisComida(PoblacionBacterias poblacion) {
+        List<Integer> dosisComida = new ArrayList<>();
+        for (int i = 0; i < poblacion.getDuracion(); i++) {
+            if (i % 2 == 0) {
+                dosisComida.add(poblacion.getDosisComida().get(0));
+            } else {
+                dosisComida.add(0);
+            }
+        }
+        return dosisComida;
+    }
 }
