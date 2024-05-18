5851a49d1dcb18c5611b6f873890ce5d13305412
KravAD <dkrav@myuax.com>
2024-05-18 18:54:41 +0200

    commit 23
    Añado getters y setter de la clase Celda.

--- CultivoBacteria2/src/main/java/Montecarlo/Celda.java
+++ CultivoBacteria2/src/main/java/Montecarlo/Celda.java
@@ -11,4 +11,20 @@ public class Celda {
         this.comida = comida;
         this.bacterias = new ArrayList<Bacteria>();
     }
+
+    public int getComida() {
+        return comida;
+    }
+
+    public void setComida(int comida) {
+        this.comida = comida;
+    }
+
+    public List<Bacteria> getBacterias() {
+        return bacterias;
+    }
+
+    public void setBacterias(List<Bacteria> bacterias) {
+        this.bacterias = bacterias;
+    }
 }
