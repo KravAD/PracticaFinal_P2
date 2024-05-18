97e60ba2044ac109111342175a468e977055a7ae
KravAD <dkrav@myuax.com>
2024-05-18 22:21:53 +0200

    commit 33
    Estructura de la ventana con dos botones para iniciar y reiniciar la simulación, area de texto para escribir el resultado y un panel para el plato 20x20.

--- CultivoBacteria2/src/main/java/IU/InterfazMontecarlo.java
+++ CultivoBacteria2/src/main/java/IU/InterfazMontecarlo.java
@@ -1,5 +1,40 @@
 package IU;
 
+import javax.swing.*;
+import java.awt.*;
+import Montecarlo.Simulacion;
+
 public class InterfazMontecarlo extends JFrame {
+    private JButton botonSimular;
+    private JButton botonReiniciar;
+    private JTextArea areaTexto;
+    private JPanel panelPlato;
+    private JLabel[][] celdas;
+    private Simulacion simulacion;
+
+    public InterfazMontecarlo(Simulacion simulacion) {
+        this.simulacion = simulacion;
+
+        botonSimular = new JButton("Iniciar simulación");
+        botonReiniciar = new JButton("Reiniciar simulación");
+        areaTexto = new JTextArea();
+        panelPlato = new JPanel(new GridLayout(20, 20));
+        celdas = new JLabel[20][20];
+
+
+
+        this.setLayout(new BoxLayout(this.getContentPane(), BoxLayout.Y_AXIS));
+
+        this.getContentPane().add(botonSimular);
+        this.getContentPane().add(botonReiniciar);
+        this.getContentPane().add(new JScrollPane(areaTexto));
+        this.getContentPane().add(panelPlato);
+
+        this.setSize(500, 500);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+        
+    }
+
 
 }
