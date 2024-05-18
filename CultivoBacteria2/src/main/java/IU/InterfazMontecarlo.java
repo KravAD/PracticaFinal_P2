71195c4298538ad9b36b17bdc85d1d629044d7b4
KravAD <dkrav@myuax.com>
2024-05-18 22:25:21 +0200

    commit 34
    Añado la acción de los botones mediante los métodos iniciar y reiniciar.

--- CultivoBacteria2/src/main/java/IU/InterfazMontecarlo.java
+++ CultivoBacteria2/src/main/java/IU/InterfazMontecarlo.java
@@ -21,7 +21,8 @@ public class InterfazMontecarlo extends JFrame {
         panelPlato = new JPanel(new GridLayout(20, 20));
         celdas = new JLabel[20][20];
 
-
+        botonSimular.addActionListener(e -> iniciarSimulacion());
+        botonReiniciar.addActionListener(e -> reiniciarSimulacion());
 
         this.setLayout(new BoxLayout(this.getContentPane(), BoxLayout.Y_AXIS));
 
@@ -33,8 +34,32 @@ public class InterfazMontecarlo extends JFrame {
         this.setSize(500, 500);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setVisible(true);
-        
+
+        for (int fila = 0; fila < 20; fila++) {
+            for (int columna = 0; columna < 20; columna++) {
+                JLabel celda = new JLabel();
+                celda.setOpaque(true);
+                celdas[fila][columna] = celda;
+                panelPlato.add(celda);
+            }
+        }
     }
 
+    private void iniciarSimulacion() {
+        simulacion.iniciarSimulacion();
+        String resultados = simulacion.finalizarSimulacion();
+        areaTexto.setText(resultados);
+        actualizarPanelPlato();
+    }
+
+    private void reiniciarSimulacion() {
+        simulacion = new Simulacion(simulacion.getDias());
+        areaTexto.setText("");
+        actualizarPanelPlato();
+    }
+
+    private void actualizarPanelPlato() {
+
+    }
 
 }
